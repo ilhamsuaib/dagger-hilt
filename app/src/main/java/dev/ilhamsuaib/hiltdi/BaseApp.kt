38a78f3c0da8a02ec4412753959f38ac094385f4
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-20 11:45:58 +0800

    implement dagger hilt

---
+++ app/src/main/java/dev/ilhamsuaib/hiltdi/BaseApp.kt
@@ -0,0 +1,11 @@
+package dev.ilhamsuaib.hiltdi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created By @ilhamsuaib on 20/06/20
+ */
+
+@HiltAndroidApp
+class BaseApp : Application()
--- app/src/main/java/dev/ilhamsuaib/hiltdi/main/MainActivity.kt
+++ app/src/main/java/dev/ilhamsuaib/hiltdi/main/MainActivity.kt
@@ -1,9 +1,15 @@
-package dev.ilhamsuaib.hiltdi
+package dev.ilhamsuaib.hiltdi.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import dev.ilhamsuaib.hiltdi.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
